604265fb96de504367203dfdf8a62534e81db2e6
s-kanwar <49239589+s-kanwar@users.noreply.github.com>
2019-05-03 21:03:09 +0530

    Add files via upload

---
+++ sample_project.js
@@ -0,0 +1,19 @@
+function clock() {
+	var fullDate = new Date();
+	var hours = fullDate.getHours();
+	var mins = fullDate.getMinutes();
+	var secs = fullDate.getSeconds();
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	if (mins < 10) {
+		mins = "0" + mins;
+	}
+	if (secs < 10) {
+		secs = "0" + secs;
+	}
+	document.getElementById('hours').innerHTML = hours;
+	document.getElementById('min').innerHTML = ":" + mins;
+	document.getElementById('sec').innerHTML = ":" + secs;
+}
+setInterval(clock, 100);
